b5dd95119b1dcf05d497245126aa33b13fd40777
nima <nima@gmail.com>
2023-07-03 12:57:50 +0330

    first coommit

---
+++ products.js
@@ -0,0 +1,22 @@
+export const products = [
+    {
+        id: 1,
+        name: "apple",
+    },
+    {
+        id: 2,
+        name: "apple1",
+    },
+    {
+        id: 3,
+        name: "apple2",
+    },
+    {
+        id: 4,
+        name: "apple3",
+    },
+    {
+        id: 5,
+        name: "apple4",
+    },
+];
---
+++ server.js
@@ -0,0 +1,33 @@
+import Fastify from "fastify";
+import { products } from "./products.js";
+
+const PORT = 5000;
+const fastify = Fastify({ logger: true });
+
+fastify.get("/", (req, reply) => {
+    reply.send({
+        message: "hello",
+    });
+});
+
+fastify.get("/products", (req, reply) => {
+    reply.send(products);
+});
+
+fastify.get("/products/:id", (req, reply) => {
+    const { id } = req.params;
+    const product = products.find((p) => p.id == id);
+    if (!product) reply.code(404).send({ message: "product not found" });
+    reply.send(product);
+});
+
+const main = async () => {
+    try {
+        await fastify.listen(PORT);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+
+main();
